27fc28c9f42a386155b5a223b7ba6bd242561db4
Shaurya Sehgal <56615472+0shaurya@users.noreply.github.com>
2022-06-14 22:49:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload=function() {
 
-	var targetTime = 1667192400000;
+	//var targetTime = 1667192400000;
 
 	const title = document.getElementById("title");
 	const days = document.getElementById("days");
@@ -8,32 +8,75 @@ window.onload=function() {
 	const minutes = document.getElementById("minutes");
 	const seconds = document.getElementById("seconds");
 
+	const daysWord = document.getElementById("daysWord");
+	const hoursword = document.getElementById("hoursWord");
+	const minutesWord = document.getElementById("minutesWord");
+	const secondsWord = document.getElementById("secondsWord");
+
 	const button1 = document.getElementById("titleButton");
 	const button2 = document.getElementById("changeBackground");
+
+	const dateInput = document.getElementById("dateInput");
+	const timeInput = document.getElementById("timeInput");
 
 	const body = document.getElementById("body");
 
 	function write() {
 		const currentDate = new Date();
+		targetTime = ((dateInput.valueAsNumber+timeInput.valueAsNumber)+(currentDate.getTimezoneOffset()*60000))
+		
+		if (!(targetTime > 0 || targetTime <1)) {
+			targetTime = 1667192400000;
+		}
+
 
 		currentTime = currentDate.getTime();
+
 		diff = (targetTime - currentTime)/1000;
-		days.textContent    = Math.floor(diff/60/60/24);
-		hours.textContent   = Math.floor((diff/60/60/24-days.textContent)*24);
-		minutes.textContent = Math.floor((diff/60/60-Math.floor(diff/60/60))*60);
-		seconds.textContent = Math.floor(diff%60);
+
+
+		if (diff <= 0) {
+			days.textContent    = " ";
+			hours.textContent   = " ";
+			minutes.textContent = " ";
+			seconds.textContent = "​";
+			daysWord.textContent = "​";
+			hoursWord.textContent = "​";
+			minutesWord.textContent = "​";
+			secondsWord.textContent = "​";
+			hereMessage.textContent = "It's here! 🥳🎈🎊";
+		} else {
+			days.textContent    = Math.floor(diff/60/60/24);
+			hours.textContent   = Math.floor((diff/60/60/24-Math.floor(diff/60/60/24))*24);
+			minutes.textContent = Math.floor((diff/60/60-Math.floor(diff/60/60))*60);
+			seconds.textContent = Math.floor(diff%60);
+			daysWord.textContent = "days";
+			hoursWord.textContent = "hours";
+			minutesWord.textContent = "minutes";
+			secondsWord.textContent = "seconds";
+			hereMessage.textContent = "​";
+		}
 	}
 	
 	write();
-	var run = setInterval(write, 1000);
+	var run = setInterval(write, 100);
 
 	button1.addEventListener("click", () => {
-		title.textContent = prompt("Title?");
-		targetTime = new Date(prompt("New date? ex Syntax: 14 Apr 2045"));
+		let x = prompt("Title?");
+		if (x === "") {
+		
+		} else if (x.trim().length === 0) {
+			title.textContent = "​";
+		} else {
+			title.textContent = x;
+		}
 	})
 
 	button2.addEventListener("click", () => {
-		body.style = "background-image: url(" + prompt("Image URL?") + ")";
+		let x = prompt("Image URL?");
+		if (x === "" || x.trim().length === 0) {
+		} else {
+		body.style = "background-image: url(" + x + ")";
+		}
 	})
 }
-
